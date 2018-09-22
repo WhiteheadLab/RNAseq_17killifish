a25c644ec51a1cb86a04b817d85abad774142100
Lisa Johnson <ljcohen@ucdavis.edu>
2018-09-22 13:14:11 -0700

    full model annotations

--- DE_scripts/full_model.R
+++ DE_scripts/full_model.R
@@ -3,15 +3,35 @@ library(RColorBrewer)
 library(gplots)
 library(tximport)
 library(lattice)
+library("Rtsne")
+library(dplyr)
+library(tidyr)
+library("biomaRt")
 source('~/Documents/UCDavis/Whitehead/RNAseq_15killifish/scripts/plotPCAWithSampleNames.R')
 source('~/Documents/UCDavis/Whitehead/RNAseq_15killifish/scripts/overLapper_original.R')
-counts <- read.csv("/Users/johnsolk/Documents/UCDavis/Whitehead/16killifish_counts_RNAseq_filtered_2July2018_test.csv",stringsAsFactors = FALSE)
-counts2 <-read.csv("~/Documents/UCDavis/Whitehead/16killifish_counts_RNAseq_filtered_2July2018_56K.csv",stringsAsFactors = FALSE)
+# This is the one with just counts
+counts <- read.csv("/Users/johnsolk/Documents/UCDavis/Whitehead/16killifish_counts_RNAseq_filtered_20September2018.csv",stringsAsFactors = FALSE)
+# This is just the counts with Experimental Design Info in the last 4 rows
+counts2 <-read.csv("~/Documents/UCDavis/Whitehead/16killifish_counts_RNAseq_filtered_20September2018_designfactors.csv",stringsAsFactors = FALSE)
 dim(counts)
+dim(counts2)
 head(counts)
+tail(counts)
+tail(counts2)
 #design<-read.csv("~/Documents/UCDavis/Whitehead/salinity_killifish_design.csv",header=TRUE)
 design <- counts2[counts2$GeneName == 'Empty',]
 design
+dim(design)
+design$type <- c("species","native_salinity","clade","group","condition")
+
+#remove transfer treatment
+transfer_samples<-design[design$type=="condition",]
+transfer_samples<-transfer_samples[, transfer_samples[1, ] == c("transfer")]
+transfer_samples<-colnames(transfer_samples)
+BW_FW_counts<-counts[, -which(colnames(counts) %in% transfer_samples)]
+dim(BW_FW_counts)
+
+# design cateogories (full)
 sp<-as.character(unlist(design[1,]))
 sp<-sp[-c(1,2)]
 ph<-as.character(unlist(design[2,]))
@@ -20,8 +40,43 @@ cl<-as.character(unlist(design[3,]))
 cl<-cl[-c(1,2)]
 de<-as.character(unlist(design[4,]))
 de<-de[-c(1,2)]
+condition<-as.character(unlist(design[5,]))
+condition<-condition[-c(1,2)]
 
-x <- counts[ -c(1,2) ]
+# design categoreis, remove "transfer" samples
+design_BW_FW <- design[, -which(colnames(design) %in% transfer_samples)]
+
+sp<-as.character(unlist(design_BW_FW[1,]))
+sp<-sp[-c(1,2)]
+sp<-sp[-91]
+length(sp)
+ph<-as.character(unlist(design_BW_FW[2,]))
+ph<-ph[-c(1,2)]
+ph<-ph[-91]
+length(ph)
+cl<-as.character(unlist(design_BW_FW[3,]))
+cl<-cl[-c(1,2)]
+cl<-cl[-91]
+length(cl)
+de<-as.character(unlist(design_BW_FW[4,]))
+de<-de[-c(1,2)]
+de<-de[-91]
+length(de)
+condition<-as.character(unlist(design_BW_FW[5,]))
+condition<-condition[-c(1,2)]
+condition<-condition[-91]
+length(condition)
+BW_FW_counts <- counts[, -which(colnames(counts) %in% transfer_samples)]
+geneID <- BW_FW_counts$GeneName
+rownames(BW_FW_counts) <- BW_FW_counts$GeneName 
+BW_FW_counts<-BW_FW_counts[ -c(1,2) ]
+
+# normal full counts
+#x <- counts[ -c(1,2) ]
+
+# ================================================
+# PCA analysis
+
 x <- x+1
 log_x<-log(x)
 colnames(log_x)
@@ -77,27 +132,92 @@ plot(pca$x[,2:3],
 legend(185,-150,legend=c("Clade 1","Clade 2","Clade 3"),col=rainbow(length(unique(fac))),cex=1.5, pch=19)
 legend(80,-150,legend=c("Brackish","Freshwater","Marine"),cex=1.5,pch=c(16, 2, 9))
 text(pca$x[,1:2], labels=names, pos=3)
+#==========================================
 
+# DESeq2 analysis
 
-geneID <- counts$GeneName
-rownames(counts) <- counts$GeneName 
-counts <- counts[-c(1)]
-counts <- counts[-c(1)]
-colnames(counts)
-  
-cols<-colnames(counts)
-ExpDesign <- data.frame(row.names=cols, physiology = ph,clade = cl)
+#rownames(counts) <- counts$GeneName 
+#counts <- counts[-c(1)]
+#counts <- counts[-c(1)]
+#colnames(counts)
+ 
+cols<-colnames(BW_FW_counts) 
+#cols<-colnames(counts)
+ExpDesign <- data.frame(row.names=cols, group = de,condition=condition)
 ExpDesign
 
-all(rownames(ExpDesign) == colnames(counts))
-counts_round<- round(counts,digits=0)
-dds <- DESeqDataSetFromMatrix(countData = counts_round,colData = ExpDesign,design = ~ clade)
-#dds <- DESeqDataSetFromTximport(countData = counts2,colData = ExpDesign,design = ~ clade)
+#all(rownames(ExpDesign) == colnames(counts))
+all(rownames(ExpDesign) == colnames(BW_FW_counts))
+counts_round<- round(BW_FW_counts,digits=0)
+#counts_round<- round(counts,digits=0)
+dds <- DESeqDataSetFromMatrix(countData = counts_round,colData = ExpDesign,design = ~ group + condition)
+
+#dds <- DESeqDataSetFromTximport(countData = counts2,colData = ExpDesign,design = ~ clade + physiology + clade:condition)
 dds<-DESeq(dds,betaPrior=FALSE)
 matrix(resultsNames(dds))
-log_cds<-rlog(dds)
+#log_cds<-rlog(dds)
 plotDispEsts(dds)
+colData(dds)
+#res <- results(dds, contrast=c("condition","15_ppt","0.2_ppt"))
+#res <- as.data.frame(res[order(res$padj),])
+#res
+res <- results(dds, tidy=TRUE, contrast=c("condition", "15_ppt", "0.2_ppt")) %>% arrange(padj) %>% tbl_df()
+
+
+geneID <- res$row
+ensembl_geneID <- startsWith(geneID,"ENS")
+ensembl_geneID<- geneID[ensembl_geneID]
+
+
+goi <- res$row[1:1000]
+stopifnot(all(goi %in% names(dds)))
+goi
+colData(dds)$physiology <- ph
+colData(dds)$clade <- cl
+tcounts <- t(log2((counts(dds[goi, ], normalized=TRUE, replaced=FALSE)+.5))) %>%
+  merge(colData(dds), ., by="row.names") %>%
+  gather(gene, expression, (ncol(.)-length(goi)+1):ncol(.))
+tcounts %>% 
+  select(Row.names, group, clade, physiology, condition, gene, expression) %>% 
+  head %>% 
+  knitr::kable()
+
+# ============================================
+# biomart annotation
+# playfish
+# ============================================
+ensembl=useMart("ensembl")
+ensembl = useDataset("xmaculatus_gene_ensembl",mart=ensembl)
+length(ensembl_geneID)
+query<-getBM(attributes=c('ensembl_gene_id','gene_biotype','external_gene_name'), filters = 'ensembl_gene_id', values = ensembl_geneID, mart=ensembl)
+
+# ============================================
+
+
+ggplot(tcounts, aes(condition, expression, fill=physiology)) + 
+  geom_boxplot() + 
+  facet_grid(~gene~clade,scales='free_y') +
+  theme_bw() +
+  theme(legend.position="bottom",panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
+  labs(x="salinity treatment", 
+       y="Expression (log normalized counts)", 
+       fill="Native Physiology")
+
+pdf("~/Documents/UCDavis/Whitehead/RNAseq_15killifish/multi-ggplot2-catalog.pdf")
+for (i in goi) {
+  p <- ggplot(filter(tcounts, gene==i), aes(condition, expression, fill=physiology)) + geom_boxplot() + 
+    facet_grid(~gene~clade,scales='free_y') +
+    theme_bw() +
+    theme(legend.position="bottom",panel.grid.major = element_blank(),panel.grid.minor = element_blank()) +
+    labs(x="salinity treatment", 
+         y="Expression (log normalized counts)", 
+         fill="Native Physiology")
+  print(p)
+}
+dev.off()
+
 plotPCAWithSampleNames(log_cds,intgroup="clade",ntop=40000)
+
 
 counts_table = counts(dds, normalized=TRUE )
 dim(counts_table)
@@ -106,7 +226,6 @@ dim(filtered_norm_counts)
 filtered_norm_counts<-as.data.frame(filtered_norm_counts)
 GeneID<-rownames(filtered_norm_counts)
 filtered_norm_counts<-cbind(filtered_norm_counts,GeneID)
-
 
 res.1<-results(dds,contrast=c("clade","Clade1","Clade2"))
 dim(res.1)
---
+++ scripts/tSNE.R
@@ -0,0 +1,45 @@
+# remove F.het.MDPP and F.het.MDPL and F.grandis from PCA
+# what are % PCA
+library(ggplot2)
+
+design
+sp<-as.character(unlist(design[1,]))
+sp<-sp[-c(1,2)]
+ph<-as.character(unlist(design[2,]))
+ph<-ph[-c(1,2)]
+cl<-as.character(unlist(design[3,]))
+cl<-cl[-c(1,2)]
+de<-as.character(unlist(design[4,]))
+de<-de[-c(1,2)]
+# clade
+names<-colnames(log_x)
+tsne<-Rtsne(t(log_x),dims=2,perplexity=10,verbose=T,max_iter=1000)
+tplot<-cbind(v1=tsne$Y[,1],v2=tsne$Y[,2],clade)
+a<-as.data.frame(tplot)
+ggplot(a,
+       aes(x=v1,y=v2,color=cl,label=names))+
+  geom_point(cex=3) +
+  geom_text(aes(label=names),hjust=0,vjust=2)+
+  theme_classic() +
+  scale_color_brewer(type="seq",palette = "Set1") +
+  labs(x="tsne1",y="tsne2")+
+  theme(axis.line=element_line(size=1.5),
+        axis.title = element_text(size=20),
+        legend.text = element_text(size=20),
+        legend.title = element_text(size=20),
+        axis.text=element_text(size=15))
+# genes
+set.seed(5)
+tsne<-Rtsne(log_x,dims=2,perplexity=50,verbose=T,max_iter=1000,check_duplicates = FALSE)
+tplot<-cbind(v1=tsne$Y[,1],v2=tsne$Y[,2])
+a<-as.data.frame(tplot)
+ggplot(a,
+       aes(x=v1,y=v2))+
+  geom_point(cex=1) +
+  theme_classic() +
+  labs(x="tsne1",y="tsne2")+
+  theme(axis.line=element_line(size=1.5),
+        axis.title = element_text(size=20),
+        legend.text = element_text(size=20),
+        legend.title = element_text(size=20),
+        axis.text=element_text(size=15))
